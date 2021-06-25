6e6e762c5693478c02a4f2d9559a33709301242b
ryanfuhrman <rsfuhrman10@gmail.com>
2021-06-25 14:35:41 -0400

    Added Person schema

--- myApp.js
+++ myApp.js
@@ -1,7 +1,18 @@
 require('dotenv').config();
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
 
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
-let Person;
+const personSchema = new Schema({
+  name: {type: String, required: true},
+  age: Number,
+  favoriteFoots: [{body: String}]
+});
+
+let Person = mongoose.model('Person', personSchema);;
+
+console.log(Person);
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
